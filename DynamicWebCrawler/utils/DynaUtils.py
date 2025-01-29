6b79a98c747264fdbcb6d3f4e3981f2926ed68dd
thinkspace <igrainrain@163.com>
2025-01-29 20:08:47 +0800

    update commit

--- DynamicWebCrawler/utils/DynaUtils.py
+++ DynamicWebCrawler/utils/DynaUtils.py
@@ -24,16 +24,27 @@ class Utils:
             os.system("pkill -9 chromedriver")
 
     @staticmethod
-    def init_broswer_driver():
+        def init_broswer_driver():
         settings = get_project_settings()
         # chrome_driver_path = settings.get('CHROME_DRIVER_PATH')
         options = webdriver.ChromeOptions()
         # options.add_argument('--start-maximized')  # 浏览器窗口最大化
         options.page_load_strategy = 'eager'
         options.add_experimental_option('detach', False)
-        options.binary_location = r'chrome/chrome.exe'
-        # options.add_argument("--headless")
-        # options.add_argument("--disable-gpu")
-        # options.add_argument("--no-sandbox")
+
+        if Utils.is_windows_os():
+            options.binary_location = r'C:\local\bin\chrome\chrome.exe'
+        else:
+            options.binary_location = '/usr/bin/google-chrome'
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
         options.add_argument('blink-settings=imagesEnabled=false')
-        return webdriver.Chrome(service=Service('chromedriver.exe'), options=options)
+        options.add_argument('--disable-images')
+        options.add_argument('--disable-javascript')
+        options.add_argument('--log-level=3')  # 禁用大多数日志
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])  # 禁用控制台日志
+        if Utils.is_windows_os():
+            return webdriver.Chrome(service=Service(r'C:\local\bin\chromedriver.exe'), options=options)
+        else:
+            return webdriver.Chrome(service=Service('/usr/local/bin/chromedriver'), options=options)
